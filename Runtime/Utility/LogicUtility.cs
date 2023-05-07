301e0206f535e4a84fa165316ea195bb90f103f2
ryota-mac <cool.sn.115@gmail.com>
2023-05-08 00:54:22 +0900

    確率計算ロジックを追加、スロー処理用の処理を追加

---
+++ Runtime/Utility/LogicUtility.cs
@@ -0,0 +1,54 @@
+﻿
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MyFw
+{
+    /// <summary>
+    /// 確率インターフェース.
+    /// </summary>
+    public interface IProbability
+    {
+        int Probability { get; }
+    }
+
+    /// <summary>
+    /// ロジックユーティリティ
+    /// </summary>
+    public static class LogicUtility
+    {
+        /// <summary>
+        /// 確率を持ったリストデータから抽選で一つピックする.
+        /// </summary>
+        /// <param name="classList">確率リスト</param>
+        /// <returns>ピックされた１データ</returns>
+        public static Class CalcLottery<Class>(IEnumerable<Class> classList)
+            where Class : IProbability
+        {
+            if (!classList.Any())
+            {
+                return default;
+            }
+
+            if (classList.Count() == 1)
+            {
+                return classList.First();
+            }
+
+            var total = classList.Sum(c => c.Probability);
+            var sorted = classList.OrderBy(c => c.Probability);
+            var value = UnityEngine.Random.Range(0, total);
+
+            foreach (var c in sorted)
+            {
+                if (c.Probability >= value)
+                {
+                    return c;
+                }
+                value -= c.Probability;
+            }
+
+            return sorted.Last();
+        }
+    }
+}
---
+++ Runtime/Utility/TimeScaleContextInstaller.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using UnityEngine;
+using Zenject;
+using UniRx;
+
+namespace MyFw
+{
+    /// <summary>
+    /// timescale変更インターフェース.
+    /// </summary>
+    public interface ITimeScaler
+    {
+        float RequestTimeScale(string key);
+        void ForceReset();
+    }
+
+    /// <summary>
+    /// TimeScale設定クラス
+    /// </summary>
+    [CreateAssetMenu(fileName = "TimeScaleContextInstaller", menuName = "Installers/TimeScaleContextInstaller")]
+    public class TimeScaleContextInstaller : ScriptableObjectInstaller
+    {
+        /// <summary>
+        /// 設定配列.
+        /// </summary>
+        [SerializeField] private List<TimeScaleContext> contextList = new();
+
+        /// <summary>
+        /// インストール実行
+        /// </summary>
+        public override void InstallBindings()
+        {
+            Container.Bind<IEnumerable<TimeScaleContext>>().FromInstance(this.contextList);
+            Container.BindInterfacesTo<TimeScalePresenter>().FromNew().AsSingle().NonLazy();
+        }
+
+        /// <summary>
+        /// TimeScale定義データ.
+        /// </summary>
+        [Serializable]
+        private class TimeScaleContext
+        {
+            [SerializeField] string key;
+            [SerializeField] float timeScale;
+            [SerializeField] float runningTime;
+
+            public string Key => this.key;
+            public float TimeScale => this.timeScale;
+            public float RunningTime => this.runningTime;
+        }
+
+        /// <summary>
+        /// TimeScalePresenterBase
+        /// </summary>
+        private class TimeScalePresenter : ITimeScaler, IDisposable
+        {
+            private readonly IEnumerable<TimeScaleContext> contextList;
+            private IDisposable timerDisposable;
+
+            [Inject]
+            public TimeScalePresenter(
+                IEnumerable<TimeScaleContext> contextList
+                )
+            {
+                this.contextList = contextList;
+            }
+
+            public float RequestTimeScale(string key)
+            {
+                var data = this.contextList.FirstOrDefault(x => x.Key == key);
+                if (data == null)
+                {
+                    return 0;
+                }
+
+                var execTime = data.TimeScale * data.RunningTime;
+                Time.timeScale = data.TimeScale;
+
+                this.timerDisposable?.Dispose();
+                this.timerDisposable = Observable.Timer(TimeSpan.FromSeconds(execTime))
+                    .Subscribe(_ => Time.timeScale = 1.0f);
+
+                return execTime;
+            }
+
+            public void ForceReset()
+            {
+                this.timerDisposable?.Dispose();
+            }
+
+            public void Dispose()
+            {
+                this.timerDisposable?.Dispose();
+            }
+        }
+    }
+}
